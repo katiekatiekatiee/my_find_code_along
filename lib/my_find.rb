533fdd355e21c4734d33fb7d1533c7799fa4ea52
Catherine Hodgkinson <catherinemhodgkinson@gmail.com>
2021-03-04 22:35:33 -0500

    commit tree cd00955ba38329dbd7acb9131c4ecda37899e8c6

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,11 @@
 require 'pry'
 
 def my_find(collection)
-
+    i = 0
+    while i < collection.length
+        if yield(collection[i]) #lines 6 adn 7 can also be written on one line: return collection[i] if yield(collection[i])
+            return collection[i]
+        end
+            i = i + 1
+    end
 end
